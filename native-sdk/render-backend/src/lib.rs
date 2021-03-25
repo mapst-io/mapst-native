b9415d7328481cf2ce9f446d99f49d30f126b3d7
Yaroslav Biletskyi <yaroslav.biletskyi.96@gmail.com>
2021-03-25 16:12:26 +0200

    added numeric limits for basic types, added methods for point creation

--- native-sdk/render-backend/src/lib.rs
+++ native-sdk/render-backend/src/lib.rs
@@ -1,7 +1,22 @@
+use geometry;
+use geometry::Point;
+use std::any::Any;
+
+
+fn test() {
+    let point: Point<i64> = Point::max();
+    let d = point.clone();
+    let k = point;
+    let s = Point::new(111, 2222);
+}
+
 #[cfg(test)]
 mod tests {
+    use super::test;
+
     #[test]
     fn it_works() {
+        test();
         assert_eq!(2 + 2, 4);
     }
 }
